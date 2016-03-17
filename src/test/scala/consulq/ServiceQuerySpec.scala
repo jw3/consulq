680bcab5117cc46a8edc992bbb2d0c37bba3a6e0
John Wass <jwass3@gmail.com>
2016-03-16 21:26:55 -0400

    disable dns for travis - not an issue right now, dns isnt utilized outside container

--- src/test/scala/consulq/ServiceQuerySpec.scala
+++ src/test/scala/consulq/ServiceQuerySpec.scala
@@ -8,13 +8,13 @@ import org.scalatest.{AsyncWordSpecLike, Matchers}
 /**
  *
  */
-class QueryConsul extends TestKit(ActorSystem()) with AsyncWordSpecLike with ImplicitSender with Matchers {
+class ServiceQuerySpec extends TestKit(ActorSystem()) with AsyncWordSpecLike with ImplicitSender with Matchers {
   implicit val mat = ActorMaterializer()
 
   "queries" should {
     "list all services" in {
       ConsulQuery().services().map { res =>
-        res.size shouldBe 7
+        res.size shouldBe 5
       }
     }
 
